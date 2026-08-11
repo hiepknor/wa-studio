526ced817bcdd45cf09d9b0286fbf243e206fd8d
hiepknor <hiepknor@gmail.com>
2026-08-12 01:04:38 +0700

    style: add shared Warp scrollbar treatment

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import "./fonts.css";
 import "./styles/tokens.css";
+import "./styles/scrollbars.css";
 import { App } from "./app/App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
